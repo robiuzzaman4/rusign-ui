89e0e1a8f14ab43b99628b57d694ef0db3a8d545
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-06-26 22:51:28 +0600

    feat: initialized project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,39 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        border: "hsl(var(--ru-border))",
+        input: "hsl(var(--ru-input))",
+        ring: "hsl(var(--ru-ring))",
+        background: "hsl(var(--ru-background))",
+        foreground: "hsl(var(--ru-foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--ru-primary))",
+          foreground: "hsl(var(--ru-primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--ru-secondary))",
+          foreground: "hsl(var(--ru-secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--ru-destructive))",
+          foreground: "hsl(var(--ru-destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--ru-muted))",
+          foreground: "hsl(var(--ru-muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--ru-accent))",
+          foreground: "hsl(var(--ru-accent-foreground))",
+        },
       },
     },
   },
